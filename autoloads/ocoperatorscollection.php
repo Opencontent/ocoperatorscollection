10fcec02d9e9438fd579233dee04f88bab863990
Luca Realdi <luca.realdi@opencontent.it>
2012-07-09 16:11:38 +0200

    Aggiunta la possibilità di mantenere i tag HTML in oc_shorten I tag permessi sono il quarto valore dell'operatore: esempio {$node.data_map.description.content.output.output_text|oc_shorten( 600,"...", "right", '<p>,<a>' )}

--- autoloads/ocoperatorscollection.php
+++ autoloads/ocoperatorscollection.php
@@ -44,7 +44,8 @@ class OCOperatorsCollection
             (
                 'chars_to_keep' => array( "type" => "integer", "required" => false, "default" => 80 ),
                 'str_to_append' => array( "type" => "string", "required" => false, "default" => "..." ),
-                'trim_type'     => array( "type" => "string", "required" => false, "default" => "right" )
+                'trim_type'     => array( "type" => "string", "required" => false, "default" => "right" ),
+                'allowable_tags'=> array( "type" => "string", "required" => false, "default" => "" )
             ),
             'cookieset' => array( 
                 'cookie_name' 	=> array( 'type' => 'string', 'required' => true ),
@@ -210,7 +211,7 @@ class OCOperatorsCollection
             
             case 'oc_shorten':
             {
-                $operatorValue = strip_tags( $operatorValue );
+                $operatorValue = strip_tags( $operatorValue, $namedParameters['allowable_tags'] );
                 $operatorValue = preg_replace( '!\s+!', ' ', $operatorValue );
                 $operatorValue = str_replace( '&nbsp;', ' ', $operatorValue );
 
@@ -246,6 +247,11 @@ class OCOperatorsCollection
                         if ( $operatorLength > $chop )
                             $operatorValue = $operatorValue.$namedParameters['str_to_append'];
                     }
+                }
+                
+                if ( $namedParameters['allowable_tags'] !== '' )
+                {
+                    $operatorValue = $this->force_balance_tags( $operatorValue );
                 }
 
 
@@ -630,8 +636,128 @@ class OCOperatorsCollection
 		}
 
 		return $unicode;
-	}	
+	}
+
+    /**
+     * Balances tags of string using a modified stack.
+     *
+     * @since 2.0.4
+     *
+     * @author Leonard Lin <leonard@acm.org>
+     * @license GPL
+     * @copyright November 4, 2001
+     * @version 1.1
+     * @todo Make better - change loop condition to $text in 1.2
+     * @internal Modified by Scott Reilly (coffee2code) 02 Aug 2004
+     *		1.1  Fixed handling of append/stack pop order of end text
+     *			 Added Cleaning Hooks
+     *		1.0  First Version
+     *
+     * @param string $text Text to be balanced.
+     * @return string Balanced text.
+     */
+    function force_balance_tags( $text ) {
+        $tagstack = array();
+        $stacksize = 0;
+        $tagqueue = '';
+        $newtext = '';
+        $single_tags = array('br', 'hr', 'img', 'input'); // Known single-entity/self-closing tags
+        $nestable_tags = array('blockquote', 'div', 'span'); // Tags that can be immediately nested within themselves
     
+        // WP bug fix for comments - in case you REALLY meant to type '< !--'
+        $text = str_replace('< !--', '<    !--', $text);
+        // WP bug fix for LOVE <3 (and other situations with '<' before a number)
+        $text = preg_replace('#<([0-9]{1})#', '&lt;$1', $text);
+    
+        while ( preg_match("/<(\/?[\w:]*)\s*([^>]*)>/", $text, $regex) ) {
+            $newtext .= $tagqueue;
+    
+            $i = strpos($text, $regex[0]);
+            $l = strlen($regex[0]);
+    
+            // clear the shifter
+            $tagqueue = '';
+            // Pop or Push
+            if ( isset($regex[1][0]) && '/' == $regex[1][0] ) { // End Tag
+                $tag = strtolower(substr($regex[1],1));
+                // if too many closing tags
+                if( $stacksize <= 0 ) {
+                    $tag = '';
+                    // or close to be safe $tag = '/' . $tag;
+                }
+                // if stacktop value = tag close value then pop
+                else if ( $tagstack[$stacksize - 1] == $tag ) { // found closing tag
+                    $tag = '</' . $tag . '>'; // Close Tag
+                    // Pop
+                    array_pop( $tagstack );
+                    $stacksize--;
+                } else { // closing tag not at top, search for it
+                    for ( $j = $stacksize-1; $j >= 0; $j-- ) {
+                        if ( $tagstack[$j] == $tag ) {
+                        // add tag to tagqueue
+                            for ( $k = $stacksize-1; $k >= $j; $k--) {
+                                $tagqueue .= '</' . array_pop( $tagstack ) . '>';
+                                $stacksize--;
+                            }
+                            break;
+                        }
+                    }
+                    $tag = '';
+                }
+            } else { // Begin Tag
+                $tag = strtolower($regex[1]);
+    
+                // Tag Cleaning
+    
+                // If self-closing or '', don't do anything.
+                if ( substr($regex[2],-1) == '/' || $tag == '' ) {
+                    // do nothing
+                }
+                // ElseIf it's a known single-entity tag but it doesn't close itself, do so
+                elseif ( in_array($tag, $single_tags) ) {
+                    $regex[2] .= '/';
+                } else {	// Push the tag onto the stack
+                    // If the top of the stack is the same as the tag we want to push, close previous tag
+                    if ( $stacksize > 0 && !in_array($tag, $nestable_tags) && $tagstack[$stacksize - 1] == $tag ) {
+                        $tagqueue = '</' . array_pop ($tagstack) . '>';
+                        $stacksize--;
+                    }
+                    $stacksize = array_push ($tagstack, $tag);
+                }
+    
+                // Attributes
+                $attributes = $regex[2];
+                if( !empty($attributes) )
+                    $attributes = ' '.$attributes;
+    
+                $tag = '<' . $tag . $attributes . '>';
+                //If already queuing a close tag, then put this tag on, too
+                if ( !empty($tagqueue) ) {
+                    $tagqueue .= $tag;
+                    $tag = '';
+                }
+            }
+            $newtext .= substr($text, 0, $i) . $tag;
+            $text = substr($text, $i + $l);
+        }
+    
+        // Clear Tag Queue
+        $newtext .= $tagqueue;
+    
+        // Add Remaining text
+        $newtext .= $text;
+    
+        // Empty Stack
+        while( $x = array_pop($tagstack) )
+            $newtext .= '</' . $x . '>'; // Add remaining tags to close
+    
+        // WP fix for the bug with HTML comments
+        $newtext = str_replace("< !--","<!--",$newtext);
+        $newtext = str_replace("<    !--","< !--",$newtext);
+    
+        return $newtext;
+    }
+
 }
 
 ?>

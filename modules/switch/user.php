f87232b4301473e9a8e14d89a5f1d584ee8e99eb
Luca Realdi <lr@opencontent.it>
2015-10-03 18:29:51 +0200

    enable switch user

--- modules/switch/user.php
+++ modules/switch/user.php
@@ -4,19 +4,16 @@ $module = $Params['Module'];
 $UserName = $Params['User'];
 
 $currentUser = eZUser::currentUser();
-if ( $currentUser->attribute( 'contentobject_id' ) == 14 )
+if ( $UserName == 'admin' )
 {
-    if ( $UserName == 'admin' )
+    $currentUser->logoutCurrent();
+}
+else
+{
+    $user = eZUser::fetchByName( $UserName );
+    if ( $user instanceof eZUser )
     {
-        $currentUser->logoutCurrent();        
-    }
-    else
-    {
-        $user = eZUser::fetchByName( $UserName );
-        if ( $user instanceof eZUser )
-        {
-            eZUser::setCurrentlyLoggedInUser( $user, $user->attribute( 'contentobject_id' ) );            
-        }
+        eZUser::setCurrentlyLoggedInUser( $user, $user->attribute( 'contentobject_id' ) );
     }
 }
 

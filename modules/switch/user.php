ab9821aa8d16711c34e487d0ef2faab89b729ae5
Luca Realdi <lr@opencontent.it>
2019-04-08 16:19:21 +0200

    add switch/user in ezaudit

--- modules/switch/user.php
+++ modules/switch/user.php
@@ -6,6 +6,12 @@ $UserName = $Params['User'];
 $currentUser = eZUser::currentUser();
 if ( $UserName == 'admin' )
 {
+    eZAudit::writeAudit( 'user-failed-login', array(
+        'User id' => $currentUser->id(),
+        'User login' => $currentUser->attribute( 'login' ),
+        'Switch to user login' => $UserName,
+        'Comment' => 'Trying to switch to admin user' )
+    );
     $currentUser->logoutCurrent();
 }
 else
@@ -13,10 +19,22 @@ else
     $user = eZUser::fetchByName( $UserName );
     if ( $user instanceof eZUser )
     {
+        eZAudit::writeAudit( 'user-login', array(
+            'User id' => $currentUser->id(),
+            'User login' => $currentUser->attribute( 'login' ),
+            'Switch to user id' => $user->id(),
+            'Switch to user login' => $UserName,
+            'Comment' => 'Using switch/user administrative feature'
+        ) );
         eZUser::setCurrentlyLoggedInUser( $user, $user->attribute( 'contentobject_id' ) );
+    }else{
+        eZAudit::writeAudit( 'user-failed-login', array(
+            'User id' => $currentUser->id(),
+            'User login' => $currentUser->attribute( 'login' ),
+            'Switch to user login' => $UserName,
+            'Comment' => 'Trying to switch to a unknown user' )
+        );
     }
 }
 
 return $module->redirectTo( '/' );
-    
-?>

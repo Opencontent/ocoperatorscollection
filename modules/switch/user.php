b23f7f56c2701f079a086ecac06b0a407ec8f840
Luca Realdi <lr@opencontent.it>
2020-07-07 09:15:40 +0200

    Avoid switch to user with admin capabilities

--- modules/switch/user.php
+++ modules/switch/user.php
@@ -4,37 +4,34 @@ $module = $Params['Module'];
 $UserName = $Params['User'];
 
 $currentUser = eZUser::currentUser();
-if ( $UserName == 'admin' )
-{
-    eZAudit::writeAudit( 'user-failed-login', array(
-        'User id' => $currentUser->id(),
-        'User login' => $currentUser->attribute( 'login' ),
-        'Switch to user login' => $UserName,
-        'Comment' => 'Trying to switch to admin user' )
-    );
-    $currentUser->logoutCurrent();
-}
-else
-{
-    $user = eZUser::fetchByName( $UserName );
-    if ( $user instanceof eZUser )
-    {
-        eZAudit::writeAudit( 'user-login', array(
+$user = eZUser::fetchByName($UserName);
+if ($user instanceof eZUser) {
+    $accessArray = $user->accessArray();
+    if (isset($accessArray['*']['*'])) {
+        eZAudit::writeAudit('user-failed-login', array(
+                'User id' => $currentUser->id(),
+                'User login' => $currentUser->attribute('login'),
+                'Switch to user login' => $UserName,
+                'Comment' => 'Trying to switch to admin user')
+        );
+        $currentUser->logoutCurrent();
+    } else {
+        eZAudit::writeAudit('user-login', array(
             'User id' => $currentUser->id(),
-            'User login' => $currentUser->attribute( 'login' ),
+            'User login' => $currentUser->attribute('login'),
             'Switch to user id' => $user->id(),
             'Switch to user login' => $UserName,
             'Comment' => 'Using switch/user administrative feature'
-        ) );
-        eZUser::setCurrentlyLoggedInUser( $user, $user->attribute( 'contentobject_id' ) );
-    }else{
-        eZAudit::writeAudit( 'user-failed-login', array(
-            'User id' => $currentUser->id(),
-            'User login' => $currentUser->attribute( 'login' ),
-            'Switch to user login' => $UserName,
-            'Comment' => 'Trying to switch to a unknown user' )
-        );
+        ));
+        eZUser::setCurrentlyLoggedInUser($user, $user->attribute('contentobject_id'));
     }
+} else {
+    eZAudit::writeAudit('user-failed-login', array(
+            'User id' => $currentUser->id(),
+            'User login' => $currentUser->attribute('login'),
+            'Switch to user login' => $UserName,
+            'Comment' => 'Trying to switch to a unknown user')
+    );
 }
 
-return $module->redirectTo( '/' );
+return $module->redirectTo('/');
